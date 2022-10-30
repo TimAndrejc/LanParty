faa0a4438cfd54ec707e2fd801ed3f3ebfb1846f
Tim <tim.andrejc02@gmail.com>
2022-10-30 18:19:35 +0100

    login/logout changes

--- login.php
+++ login.php
@@ -14,18 +14,19 @@ require_once 'header.php';
                     <h1 class="fw-bold my-5 text-uppercase">prijava</h1>
                   </div>
                   <div class="form-outline form-white mb-4">
-                    <input type="email" name="email" id="typeEmail" class="form-control form-control-lg active" />
+                    <input type="email" name="email" id="typeEmail" autocomplete="email" placeholder="Napiši email ..." class="form-control form-control-lg active" />
                     <label class="form-label" for="typeEmail">Email</label>
                   </div>
                   <div class="form-outline form-white mb-4">
-                    <input type="password" id="typePassword" name="password" class="form-control form-control-lg active" />
-                    <label class="form-label" for="typePassword">Password</label>
+                    <input type="password" id="typePassword" placeholder="Napiši geslo ..." name="password" class="form-control form-control-lg active" />
+                    <label class="form-label" for="typePassword">Geslo</label>
                   </div>
                   <div class="text-center py-5">
-                    <button class="btn btn-light btn-lg btn-rounded px-5" type="submit">Login</button>
+                    <button class="btn btn-light btn-lg btn-rounded px-5" type="submit">Prijava</button>
                   </div>
                 </div>
                 <div class="text-center">
+                <p class="mb-0"><a href="register.php" class="text-white fw-bold">Še nimaš računa?</a></p>
                   <p class="mb-0"><a href="#!" class="text-white fw-bold">Pozabljeno geslo?</a></p>
                 </div>
 
