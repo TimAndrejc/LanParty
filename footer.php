898c4eb271533f6648644f1e50bacbf1c787b6ef
Tim <tim.andrejc02@gmail.com>
2022-12-14 12:46:22 +0100

    +footer

--- footer.php
+++ footer.php
@@ -1,2 +1,55 @@
+<p>
+
+
+</p>
+
+
+      </div><!-- close topcontent-->
+    </div><!-- close content-->
+  </div>   <!-- close middle panel -->
+</div>  <!-- close col-sm-8 -->
+
+            <div class='col-xs-12 col-sm-4' >
+                <div id="right-panel">
+
+                    <div class="spacer-0"> </div>
+
+                    <div id="modules-holder"></div>
+                </div>
+            </div>  <!-- close col 4 -->
+        </div>  <!-- close row  main-content -->
+
+
+<!-- include sponsors   -->
+<p>&nbsp;</p>
+<h3 style="text-align: center;">Zahvala našim sponzorjem!<br /><br /></h3>
+<div class="row" style="text-align: center; vertical-align:middle;">
+  <div class="col-xs-12 col-md-3" style="padding: 5px">
+    <div class="element">
+      <a href ="https://www.redbull.com" ><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 64px;width: 140px;" src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f5/RedBullEnergyDrink.svg/1200px-RedBullEnergyDrink.svg.png" alt="Red Bull"></a>
+    </div>
+  </div>
+  <div class="col-xs-12 col-md-3" style="padding: 5px">
+    <div class="element">
+      <a href ="https://gorenjegostinstvo.si/"><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 64px;width: 256px;" src="https://gorenjegostinstvo.si/wp-content/uploads/2017/04/logon.png" alt="Gorenje Gostinjstvo"></a>
+    </div>
+  </div>
+  <div class="col-xs-12 col-md-3" style="padding: 5px">
+    <div class="element">
+      <a href ="https://www.in-put.si/" target="_blank"><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 64px; width: 140px;" src="https://www.in-put.si/templates/default/images/logo.png" alt="In-put"></a>
+    </div>
+  </div>
+  <div class="col-xs-12 col-md-3" style="padding: 5px">
+    <div class="element">
+      <a href ="http://blackbaud.com/" target="_blank"><img style="position: relative;  top: 50%;  transform: translateY(-50%);" src="//cdn-flightdec.userfirst.co.nz/uploads/sites/finz/files/Sponsors/blackbaud.png" alt="Visit Blackbaud"></a>
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+<!-- end footer -->
+<!-- theme includes if required -->
 </body>
 </html> 
