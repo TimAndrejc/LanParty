2f6b3e4e6e46022dfc45061cc898923cd63ee2e9
Tim <tim.andrejc02@gmail.com>
2022-12-14 11:36:05 +0100

    +footer WIP

--- footer.php
+++ footer.php
@@ -1,3 +1,68 @@
-</body>
+<footer class="text-center text-white">
+<hr>
+  <div class="container p-4">
+    <!-- Section: Images -->
+    <section class="">
+      <div class="row">
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img style ="max-height 100px; width: auto;"
+              src="https://1000logos.net/wp-content/uploads/2021/04/Red-Bull-logo.png"
+              class="w-100"
+            />
+            <a href="www.redbull.com">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://gorenjegostinstvo.si/wp-content/uploads/2017/04/logon.png"
+              class="w-100"
+            />
+            <a href="https://gorenjegostinstvo.si/">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="https://www.in-put.si/templates/default/images/logo.png"
+              class="w-100"
+            />
+            <a href="https://www.in-put.si/">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Section: Images -->
+  </div>
+  <!-- Grid container -->
 
+</footer>
+
+
+</body>
 </html> 
