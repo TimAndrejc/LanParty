9ab49250a78baba1ce05e655a529b8dac3c5f5d6
Tim <tim.andrejc02@gmail.com>
2022-12-15 10:47:50 +0100

    +changed from info to contact page

---
+++ contact.php
@@ -0,0 +1,28 @@
+<?php include_once 'header.php'; ?>
+<section class="intro">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5" style="margin-bottom:2rem">
+            <div class="card gradient-custom" style="border-radius: 1rem;">
+              <div class="card-body p-5 text-white" style="padding:0">
+                <div class="my-md-5">
+                  <div class="text-center pt-1">
+                   
+                  <i class="bi bi-info-circle-fill fa-3x"></i><br>
+                    <h1 class="fw-bold my-2">Potrebuješ pomoč?</h1>
+                    <section>Pridruži se v naš <a href="https://discord.gg/nE3dSVqMma" style="text-decoration: none; color: white; text-decoration: underline;">Discord</a> strežnik 
+                    <br>ali <br> nam piši na mail: <a href="mailto:lanparty@scv.si" style="text-decoration: none; color: white; text-decoration: underline;"> lanparty@scv.si </a>
+                </section>
+                </div>
+              </div>
+            <div style ="text-align:center; margin: 0 auto">
+            </div>
+</div>
+          </div>
+        </div>
+    </div>
+  </div>
+  </section>
+
+
+<?php include_once 'footer.php'; ?>
