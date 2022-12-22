e35aa11f16a27b61767fc324e5303ea0530caa71
Tim <tim.andrejc02@gmail.com>
2022-12-22 08:31:15 +0100

    +removed unconfirmed sponsors

--- footer.php
+++ footer.php
@@ -1,27 +1,15 @@
-
+<br>
 <h3 style="text-align: center; color:white;">Zahvala našim sponzorjem!<br /><br /></h3>
 <div class="row" style="text-align: center; vertical-align:middle; width:100%">
-  <div class="col-xs-12 col-md-3" style="padding: 5px">
+  <div class="col-xs-12 col-md-6" style="padding: 5px">
     <div class="element">
       <a href ="https://www.redbull.com" ><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 100px;width: 180px;" src="https://resources.redbull.com/logos/redbullcom/v3/redbullcom-logo.svg" alt="Red Bull"></a>
     </div>
   </div>
-  <div class="col-xs-12 col-md-3" style="padding: 5px">
+  <div class="col-xs-12 col-md-6" style="padding: 5px">
     <div class="element">
       <a href ="https://gorenjegostinstvo.si/"><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 50px;width: 260px;" src="pics/gorenje.png" alt="Gorenje Gostinjstvo"></a>
     </div>
-  </div>
-  <div class="col-xs-12 col-md-3" style="padding: 5px">
-    <div class="element">
-      <a href ="https://www.in-put.si/" target="_blank"><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 64px; width: 140px;" src="https://www.in-put.si/templates/default/images/logo.png" alt="In-put"></a>
-    </div>
-  </div>
-  
-  <div class="col-xs-12 col-md-3" style="padding: 5px">
-    <div class="element">
-      <a href ="http://blackbaud.com/" target="_blank"><img style="position: relative;  top: 50%;  transform: translateY(-50%);" src="//cdn-flightdec.userfirst.co.nz/uploads/sites/finz/files/Sponsors/blackbaud.png" alt="Visit Blackbaud"></a>
-    </div>
-  </div>
       </div>
 </div>
 </body>
