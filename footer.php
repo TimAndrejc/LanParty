dd2dbaaa41cfc8f510006ae67ac50c2dda36f1c1
Tim Andrejc <tim.andrejc02@gmail.com>
2023-01-05 19:56:09 +0100

    +added sponsors

--- footer.php
+++ footer.php
@@ -1,16 +1,31 @@
 <br>
 <h3 style="text-align: center; color:white;">Zahvala našim sponzorjem!<br /><br /></h3>
 <div class="row" style="text-align: center; vertical-align:middle; width:100%">
-  <div class="col-xs-12 col-md-6" style="padding: 5px">
+  <div class="col-xs-12 col-md-3" style="padding: 10px">
     <div class="element">
-      <a href ="https://www.redbull.com" ><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 100px;width: 180px;" src="https://resources.redbull.com/logos/redbullcom/v3/redbullcom-logo.svg" alt="Red Bull"></a>
+      <a href ="https://www.redbull.com" ><img style="position: relative;  top: 50%;  transform: translateY(-50%); max-height: 100px; max-width: 180px;" src="https://resources.redbull.com/logos/redbullcom/v3/redbullcom-logo.svg" alt="Red Bull"></a>
     </div>
   </div>
-  <div class="col-xs-12 col-md-6" style="padding: 5px">
+  <div class="col-xs-12 col-md-3" style="padding: 10px">
     <div class="element">
-      <a href ="https://gorenjegostinstvo.si/"><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 50px;width: 260px;" src="pics/gorenje.png" alt="Gorenje Gostinjstvo"></a>
+      <a href ="https://gorenjegostinstvo.si/"><img style="position: relative;  top: 50%;  transform: translateY(-50%); max-height:58 px;width: 265px;" src="pics/gorenje.png" alt="Gorenje Gostinjstvo"></a>
     </div>
-      </div>
+  </div>
+  <div class="col-xs-12 col-md-3" style="padding: 10px">
+    <div class="element">
+      <a href ="https://lenovo.com/si/"><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 50px; max-width: 260px;" src="pics/lenovo.png" alt="Legion by Lenovo"></a>
+    </div>
+  </div>
+  <div class="col-xs-12 col-md-3" style="padding: 10px">
+    <div class="element">
+      <a href ="https://uvi.gg/sl/"><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 70px; max-width: 260px;" src="pics/uvi.png" alt="UVI Gear"></a>
+    </div>
+  </div>
+  <div class="col-xs-12 col-md-3" style="padding: 10px; margin: 0 auto;">
+    <div class="element">
+      <a href ="https://sasainkubator.si/"><img style="position: relative;  top: 50%;  transform: translateY(-50%); height: 70px; max-width: 260px;" src="pics/sasainkubator.png" alt="SAŠA Inkubator"></a>
+    </div>
+  </div>
 </div>
 </body>
 </html> 
